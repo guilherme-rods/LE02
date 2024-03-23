14a58725ade233a9aac8b8098456f9dbbd22585d
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-03-22 21:03:20 -0300

    commit edição programa

--- Programa.java
+++ Programa.java
@@ -1,23 +1,70 @@
+import java.util.Scanner;
+
 public class Programa {
 
     public static void main(String[] args) {
-        
-        Ex01.executar();
-        Ex02.executar();
-        Ex03.executar();
-        Ex04.executar();
-        Ex05.executar();
-        Ex06.executar();
-        Ex07.executar();
-        Ex08.executar();
-        Ex09.executar();
-        Ex10.executar();
-        Ex11.executar();
-        Ex12.executar();
-        Ex13.executar();
-        Ex14.executar();
-        Ex15.executar();
 
+        System.out.println("LE02 - Guilherme Rodrigues:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite qual exercício quer verificar:");
+        int exercicio = scanner.nextInt();
+
+        switch (exercicio) {
+            case 1:
+                Ex01.executar();
+                break;
+            case 2:
+                Ex02.executar();
+                break;
+            case 3:
+                Ex03.executar();
+                break;
+            case 4:
+                Ex04.executar();
+                break;
+            case 5:
+                Ex05.executar();
+                break;
+            case 6:
+                Ex06.executar();
+                break;
+            case 7:
+                Ex07.executar();
+                break;
+            case 8:
+                Ex08.executar();
+                break;
+            case 9:
+                Ex09.executar();
+                break;
+            case 10:
+                Ex10.executar();
+                break;
+            case 11:
+                Ex11.executar();
+                break;
+            case 12:
+                Ex12.executar();
+                break;
+            case 13:
+                Ex13.executar();
+                break;
+            case 14:
+                Ex14.executar();
+                break;
+            case 15:
+                Ex15.executar();
+                break;
+        
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+
+        scanner.close();
+        
     }
 
 }
